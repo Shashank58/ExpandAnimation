f904899bfe7adb2e8d9b1af263e77f55ce5bd738
Shashank M <shashankm@Shashanks-MacBook-Pro.local>
2016-11-04 14:01:11 +0530

    Add PercentView

--- app/src/main/java/shashankm/bottomnav/MainActivity.java
+++ app/src/main/java/shashankm/bottomnav/MainActivity.java
@@ -1,30 +1,87 @@
 package shashankm.bottomnav;
 
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 
+import static android.view.ViewTreeObserver.*;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static int PREVIOUS_HEIGHT = -1;
     private static boolean isOpen = false;
     private LinearLayout signUp;
-    private View textTrack, button, textView;
+    private View textTrack;
+    private View button;
+    private View activityRootView;
+    private OnGlobalLayoutListener onGlobalLayoutListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.sign_up);
+        View textView = findViewById(R.id.sign_up);
         signUp = (LinearLayout) findViewById(R.id.sign_up_layout);
         textTrack = findViewById(R.id.text_track);
         button = findViewById(R.id.button);
+        PercentView percentView = (PercentView) findViewById(R.id.percent_view);
+        percentView.setPercentage(80f);
 
         textView.setOnClickListener(this);
+
+        keyBoardChangeListener();
+    }
+
+    private void keyBoardChangeListener() {
+        activityRootView = findViewById(R.id.activity_main);
+        ViewTreeObserver viewTreeObserver = activityRootView.getViewTreeObserver();
+        viewTreeObserver.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                onGlobalLayoutListener = this;
+                //r will be populated with the coordinates of your view that area still visible.
+                activityRootView.getWindowVisibleDisplayFrame(r);
+
+                int heightDiff = activityRootView.getRootView().getHeight() - (r.bottom - r.top);
+                if (PREVIOUS_HEIGHT == -1) {
+                    PREVIOUS_HEIGHT = heightDiff;
+                }
+
+                if (heightDiff > (PREVIOUS_HEIGHT + 100)) {
+                    PREVIOUS_HEIGHT = heightDiff;
+                    textTrack.setVisibility(View.GONE);
+                    button.setVisibility(View.GONE);
+                    Log.d("NavBarController", "Keyboard is shown");
+                } else if (heightDiff < PREVIOUS_HEIGHT) {
+                    PREVIOUS_HEIGHT = heightDiff;
+                    textTrack.setVisibility(View.VISIBLE);
+                    button.setVisibility(View.VISIBLE);
+                    Log.d("NavBarController", "Keyboard is hidden");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (Build.VERSION.SDK_INT < 16) {
+            activityRootView.getViewTreeObserver()
+                    .removeGlobalOnLayoutListener(onGlobalLayoutListener);
+        } else {
+            activityRootView.getViewTreeObserver()
+                    .removeOnGlobalLayoutListener(onGlobalLayoutListener);
+        }
     }
 
     @Override
